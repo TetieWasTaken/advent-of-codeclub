31b069492a787e5e3630c6b46453a11f05c9b681
TetieWasTaken <tjvssr@gmail.com>
2024-12-30 23:02:21 +0100

    chore(finished): english note

--- src/app/finished/page.tsx
+++ src/app/finished/page.tsx
@@ -6,6 +6,10 @@ export default function FinishedPage() {
       <div className="bg-gray-800 p-6 rounded-lg shadow-md text-center">
         <h2 className="text-xl font-semibold mb-4">Het is voorbij!</h2>
         <p>De Advent of Codeclub is voorbij. Bedankt voor het meedoen!</p>
+        <p>
+          (Sorry, Advent of Codeclub already ended. If you're from High Seas,
+          please check the Github readme for screenshots.)
+        </p>
       </div>
     </div>
   );
